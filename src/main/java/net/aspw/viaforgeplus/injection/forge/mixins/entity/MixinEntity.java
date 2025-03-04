6235eb79c09fbe744d62b70e80151757ce9c77d7
ksyz <ksyz@duck.com>
2025-03-04 18:50:21 +0100

    Fix hitbox size in 1.9+

--- src/main/java/net/aspw/viaforgeplus/injection/forge/mixins/entity/MixinEntity.java
+++ src/main/java/net/aspw/viaforgeplus/injection/forge/mixins/entity/MixinEntity.java
@@ -1,11 +1,19 @@
 package net.aspw.viaforgeplus.injection.forge.mixins.entity;
 
+import net.aspw.viaforgeplus.api.ProtocolFixer;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
     @Shadow
     public boolean noClip;
+
+    @ModifyConstant(method = "getCollisionBorderSize", constant = @Constant(floatValue = 0.1F))
+    private float onLivingUpdate(float constant) {
+        return ProtocolFixer.newerThan1_8() ? 0.0F : 0.1F;
+    }
 }
